1bdf0234d7a7e6bd318a655c29e312047dab8fe5
umarqazi <umarfarooq1857@gmail.com>
2024-11-27 02:53:32 +0500

    - Docker Improvements.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-cd /var/www/html || exit
+cd /var/www || exit
 
 php artisan migrate
-php artisan cache:clear
 php artisan config:clear
+php artisan cache:clear
 php artisan view:clear
 php artisan optimize:clear
 
 # Ensure correct permissions for storage and cache
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+#chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+#find /var/www/storage /var/www/bootstrap/cache -type d -exec chmod 775 {} \;
+#find /var/www/storage /var/www/bootstrap/cache -type f -exec chmod 664 {} \;
 
 # Start cron in the background
 service cron start
